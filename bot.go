06d80fe3bee1442ce00a52ed2e33b41a38bed60b
Jason Lowenthal <jason.lowenthal@asynchrony.com>
2018-09-16 22:37:02 -0500

    Baseline quotebot. Includes twitter secrets. Dont share it

---
+++ bot.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+func main() {
+	consumerKey := os.Getenv("TWITTER_BOT_CONSUMER_KEY")
+	consumerSecret := os.Getenv("TWITTER_BOT_CONSUMER_SECRET")
+	accessToken := os.Getenv("TWITTER_BOT_ACCESS_TOKEN")
+	accessSecret := os.Getenv("TWITTER_BOT_ACCESS_SECRET")
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+
+	// OAuth1 http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Verify Credentials
+	verifyParams := &twitter.AccountVerifyParams{
+		IncludeEmail: twitter.Bool(true),
+	}
+	user, _, _ := client.Accounts.VerifyCredentials(verifyParams)
+	fmt.Printf("User's Name:%+v\n", user.ScreenName)
+}
